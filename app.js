ad467b99b78715b135d004324a377db6450f8c4c
Johnatan Gutierrez <gutierrezmjd@outlook.com>
2025-03-07 19:20:50 -0500

    creacion del array

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. 
 // Aquí deberás desarrollar la lógica para resolver el problema.
+let amigo = [];
+
+function agregarAmigo(){
+    let inputAmigo = document.getElementById("amigo");
+    let nombreAmigo = inputAmigo.value;
+
+    if(nombreAmigo == false){
+        alert("Debes ingresar un nombre");
+        return;
+    }
+    amigo.push
+}
