30a7f114040d0bac097c8caef9c88237f898eafa
Johnatan Gutierrez <gutierrezmjd@outlook.com>
2025-03-07 21:39:54 -0500

    se creo la funcion para realizar el sorteo

--- app.js
+++ app.js
@@ -20,10 +20,23 @@ function renderizarNombre(){
     let listaAmigos = document.getElementById("listaAmigos");
     listaAmigos.innerHTML = "";
 
-    for ( let i = 0; i < amigo.length; i++);{
+    for ( let i = 0; i < amigo.length; i++){
         let item = document.createElement("li");
         item.textContent = amigo[i];
         listaAmigos.appendChild(item);
     }
 
-}
+};
+
+function sortearAmigo(){
+    if (amigo.length === 0){
+        alert("Debes ingresar un amigo para sortear");
+        return;
+    }
+    let amigoSorteado = amigo[Math.floor(Math.random() * amigo.length)];
+    let resultado = document.getElementById("resltado");
+    resultado.innerHTML = (`El amigo sorteado es: ${listaAmigos}`);
+
+    let limpiarLista = document.getElementById("listaAmigos")
+    limpiarLista.innerHTML = "";
+};
